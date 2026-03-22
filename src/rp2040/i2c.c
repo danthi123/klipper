dc675f69b7c3658036cfab160310b321376ba585
Kevin O'Connor <kevin@koconnor.net>
2026-03-22 13:16:09 -0400

    rp2040: Avoid run-time divide in i2c.c
    Rework the code slightly to avoid an expensive software divide when
    calculating the rate in i2c.c .
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/rp2040/i2c.c
+++ src/rp2040/i2c.c
@@ -101,11 +101,23 @@ i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr)
 
         // See `i2c_set_baudrate` in the Pico SDK `hardware_i2c/i2c.c` file
         // for details on the calculations here.
-        if (rate > 1000000)
-            rate = 1000000; // Clamp the rate to 1Mbps
-        uint32_t period = (pclk + rate / 2) / rate;
-        uint32_t lcnt = period * 3 / 5;
-        uint32_t hcnt = period - lcnt;
+        uint32_t lcnt, hcnt;
+        if (rate >= 1000000) {
+            uint32_t i2c_rate = 1000000;
+            uint32_t period = DIV_ROUND_CLOSEST(pclk, i2c_rate);
+            lcnt = period * 3 / 5;
+            hcnt = period - lcnt;
+        } else if (rate >= 400000) {
+            uint32_t i2c_rate = 400000;
+            uint32_t period = DIV_ROUND_CLOSEST(pclk, i2c_rate);
+            lcnt = period * 3 / 5;
+            hcnt = period - lcnt;
+        } else {
+            uint32_t i2c_rate = 100000;
+            uint32_t period = DIV_ROUND_CLOSEST(pclk, i2c_rate);
+            lcnt = period * 3 / 5;
+            hcnt = period - lcnt;
+        }
         uint32_t sda_tx_hold_count = ((pclk * 3) / 10000000) + 1;
 
         i2c->fs_scl_hcnt = hcnt;
